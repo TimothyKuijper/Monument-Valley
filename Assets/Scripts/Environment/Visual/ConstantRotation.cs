14c525728a7b5f8b65187f25bb2e92c4e89f09be
Tenseven-107 <nadrichem@tanous.nl>
2026-03-11 00:17:53 +0100

    Removed accidentaly added files

--- Assets/Scripts/Environment/Visual/ConstantRotation.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class ConstantRotation : MonoBehaviour
-{
-    [SerializeField] private Vector3 rotationVector;
-
-    private void Update() => transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + rotationVector * Time.deltaTime);
-}
