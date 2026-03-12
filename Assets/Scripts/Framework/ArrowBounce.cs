aff15f669b5c3cb1209c0195481dee60e146ed37
TimothyKuijper <t.kuijper12@gmail.com>
2026-03-12 19:54:35 +0100

    yep

--- Assets/Scripts/Framework/ArrowBounce.cs
+++ Assets/Scripts/Framework/ArrowBounce.cs
@@ -4,6 +4,9 @@ public class ArrowBounce : MonoBehaviour
 {
     void FixedUpdate()
     {
-        transform.Rotate(new Vector3(0,0,0));
+        gameObject.transform.Rotate(new Vector3(0,0.7f,0));
+        var startPos = transform.position;
+        float yOffset = Mathf.Sin(Time.time * 2) * 0.01f;
+        transform.position = startPos + new Vector3(0, yOffset, 0);
     }
 }
